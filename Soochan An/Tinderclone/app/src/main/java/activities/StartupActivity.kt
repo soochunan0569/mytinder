483489ade202de32b9475233100d50d9e8c21efd
soochunan0569 <79432517+soochunan0569@users.noreply.github.com>
2021-03-04 23:30:21 -0800

    change2

--- Soochan An/Tinderclone/app/src/main/java/activities/StartupActivity.kt
+++ Soochan An/Tinderclone/app/src/main/java/activities/StartupActivity.kt
@@ -8,7 +8,7 @@ import com.example.tinderclone.R
 class StartupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_startup2)
+        setContentView(R.layout.activity_startup)
     }
     fun onLogin(v: View) {
 
